b8923af561c65de76fc565c55a09e2ce234960f4
alphacrash <sudhansu.ace@gmail.com>
2021-12-06 21:03:53 +0530

    App with ESLint-Airbnb style, Prettier and Dev Backend Configured

---
+++ budget-backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const jsonServer = require("json-server");
+
+const app = express();
+
+app.use(cors());
+
+app.use("/api", jsonServer.router("db.json"));
+app.use("/login", (req, res) => {
+  res.send({
+    token: "login",
+  });
+});
+
+app.listen(3001, () => {
+  console.log("API Server Started");
+});
